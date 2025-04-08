046489095a26b8c4b91847537caf6294a0eb2ce2
Ki Leo <92660269+kileo123@users.noreply.github.com>
2025-04-08 10:52:15 +0000

    great

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4smz@v4jr8=y0q=t=4*y55&h(5+g#)u#-=1l-6_2aqf48))&38
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'obscure-goggles-49prp77r9xvcv76-8000.app.github.dev']
 
 
 # Application definition
